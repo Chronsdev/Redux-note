0d30399aed3049e7deb40ddcddb0d2d16edbaf59
Your Name <you@example.com>
2022-04-11 05:02:05 -0430

    This is part of the fullstackopen curse, with is directed by redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import filterReducer from './reducers/filterReducer'
+import noteReducer from './reducers/noteReducer'
+
+
+	const reducer = combineReducers({
+		notes: noteReducer,
+		filter: filterReducer,
+	})
+
+	const store = createStore(reducer)
+
+	console.log(store.getState())
+
+	const root = document.getElementById('root')
+	const ReactRoot = ReactDOM.createRoot(root)
+	ReactRoot.render(
+		<Provider store={store}>
+			<App />
+		</Provider>
+		)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
